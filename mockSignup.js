95674679d60ad342301611d2a7bad5219f2f3d6d
Soumava Banerjee <soumava.rivu@gmail.com>
2020-12-19 20:19:52 +0530

    updated mockSignup

--- mockSignup.js
+++ mockSignup.js
@@ -3,7 +3,7 @@ console.log("Connected");
 $(".input-btn-ajax").click(function () {
   $(".input-field").remove();
   $(".input-btn-ajax").addClass("btn-rmv");
-  $(".input-btn-ajax").text("Submitted!");
+  $(".input-btn-ajax").text("Thanks for Subscribing!");
   console.log(
     "MailChimp configuration has been removed as implementing it with ajax would have exposed my API key (And I don't want to deploy this thing to heroku for such a small task). If you checked the console/ my github code, Congrats!"
   );
