d4043a1d2de376ce25d9c6700d761f47ec01cb2e
Soumava Banerjee <soumava.rivu@gmail.com>
2020-12-19 19:52:07 +0530

    implemented dummy subsscription action

---
+++ mockSignup.js
@@ -0,0 +1,10 @@
+console.log("Connected");
+
+$(".input-btn-ajax").click(function () {
+  $(".input-field").remove();
+  $(".input-btn-ajax").addClass("btn-rmv");
+  $(".input-btn-ajax").text("Submitted!");
+  console.log(
+    "MailChimp configuration has been removed as implementing it with ajax would have exposed my API key (And I don't want to deploy this thing to heroku for such a small task). If you checked the console/ my github code, Congrats!"
+  );
+});
